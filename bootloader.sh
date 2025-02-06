f04b4ae5a7a4eb4f8d431c8298157638b73a217a
alefnode <adriancampos@teachelp.com>
2025-02-06 13:38:04 +0100

    Upgrade after using minimal-initramfs package

--- bootloader.sh
+++ bootloader.sh
@@ -25,21 +25,25 @@ case "${DEVICE}" in
         ;;
 esac
 
+kernel_version=$(ls ${MOUNTED_IMAGE_DIR}/usr/lib/modules/)
+gzip -9 -k ${MOUNTED_IMAGE_DIR}/usr/lib/modules/${kernel_version}/Image
+
 # Create a bootimg for each variant
 for variant in ${DTB_VARIANTS}; do
     echo "Creating boot image for variant ${variant}"
 
     # Append DTB to kernel
-    cat ${MOUNTED_IMAGE_DIR}/usr/lib/modules/*-sdm845/Image.gz ${MOUNTED_IMAGE_DIR}/boot/dtb/qcom/sdm845-${DTB_VENDOR}-${variant}.dtb > /tmp/kernel-dtb
+    cat ${MOUNTED_IMAGE_DIR}/usr/lib/modules/${kernel_version}/Image.gz ${MOUNTED_IMAGE_DIR}/boot/dtb/qcom/sdm845-${DTB_VENDOR}-${variant}.dtb > /tmp/kernel-dtb
 
     # Create the bootimg as it's the only format recognized by the Android bootloader
     abootimg --create ./openSUSE-Tumbleweed-ARM-PHOSH-${DEVICE}${variant}.aarch64.boot.img -c kerneladdr=0x8000 \
         -c ramdiskaddr=0x1000000 -c secondaddr=0x0 -c tagsaddr=0x100 -c pagesize=4096 \
-        -c cmdline="BOOT_IMAGE=/boot/Image root=${ROOTPART} quiet splash" \
-        -k /tmp/kernel-dtb -r ${MOUNTED_IMAGE_DIR}/boot/initrd
+        -c cmdline="root=${ROOTPART} rootdelay=2 mobileroot=${ROOTPART} loglevel=7 splash=silent console=ttyMSM0,115200 console=tty0 BOOT_IMAGE=/boot/Image" \
+        -k /tmp/kernel-dtb -r ${MOUNTED_IMAGE_DIR}/boot/initrd-${kernel_version}
 
-    #mkbootimg --kernel ${MOUNTED_IMAGE_DIR}/boot/Image --dtb ${MOUNTED_IMAGE_DIR}/boot/dtb/qcom/sdm845-${DTB_VENDOR}-${variant}.dtb --pagesize 4096 \
-    #    --base 0x00000000 --kernel_offset 0x00008000 --second_offset 0x00f00000 --tags_offset 0x00000100 \
-    #    --cmdline "root=/dev/block/sda21" --output bootimg-${variant}.img
+    #mkbootimg --kernel /tmp/kernel-dtb --ramdisk ${initrd_file_path} --pagesize 4096 \
+    #    --base 0x0 --kernel_offset 0x8000 --second_offset 0x0 --tags_offset 0x100 --ramdisk_offset 0x1000000 \
+    #    --cmdline "BOOT_IMAGE=/boot/Image root=${ROOTPART} console=ttyMSM0,115200 loglevel=7" \
+    #    --output openSUSE-Tumbleweed-ARM-PHOSH-${DEVICE}${variant}.aarch64.boot.img
 
 done
